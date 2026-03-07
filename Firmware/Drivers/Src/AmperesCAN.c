200aaeaa2536a00caf6adb66f87e5f464129156d
micade145 <micade145@gmail.com>
2026-03-06 18:01:30 -0600

    normal can mode so i don't forget

--- Firmware/Drivers/Src/AmperesCAN.c
+++ Firmware/Drivers/Src/AmperesCAN.c
@@ -35,7 +35,7 @@ static bool MX_CAN_Init() {
     hcan1->Init.SyncJumpWidth = CAN_SJW_1TQ;
     hcan1->Init.TimeSeg1 = CAN_BS1_13TQ;
     hcan1->Init.TimeSeg2 = CAN_BS2_2TQ;
-    hcan1->Init.Mode = CAN_MODE_LOOPBACK;   // TODO: change for testing
+    hcan1->Init.Mode = CAN_MODE_NORMAL;   // TODO: change for testing
     hcan1->Init.TimeTriggeredMode = DISABLE;
     hcan1->Init.AutoBusOff = DISABLE;
     hcan1->Init.AutoWakeUp = DISABLE;
